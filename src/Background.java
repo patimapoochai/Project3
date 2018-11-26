45a689f4fca629441d6d0453dd11cdc706837456
rlaiUH <45014528+rlaiUH@users.noreply.github.com>
2018-11-26 10:57:55 -1000

    Update Background.java

--- src/Background.java
+++ src/Background.java
@@ -12,7 +12,7 @@ public class Background
 		this.RES_X = RES_X;
 		this.RES_Y = RES_Y;
 		this.wallpaper = EZ.addImage(file, this.RES_X/2, this.RES_Y/2);
-		this.wallpaper2 = EZ.addImage(file, this.RES_X/2, this.RES_Y + (this.RES_Y / 2));
+		this.wallpaper2 = EZ.addImage(file, this.RES_X/2, -this.RES_Y);
 		this.wallpaper.pushToBack();
 		this.wallpaper2.pushToBack();
 	}
@@ -21,14 +21,9 @@ public class Background
 	{
 		this.wallpaper.translateBy(0, scrollSpeed);
 		this.wallpaper2.translateBy(0, scrollSpeed);
-		if (this.wallpaper.getYCenter() >= this.RES_Y)
-			this.wallpaper.translateTo(this.RES_X/2, 0);
-		if (this.wallpaper2.getYCenter() >= this.RES_Y)
-			this.wallpaper2.translateTo(this.RES_X/2, 0);
-	}
-	
-	public void replacePlatform(ArrayList<Platform> platforms)
-	{
-		
+		if (this.wallpaper.getYCenter() >= this.RES_Y + this.RES_Y/2)
+			this.wallpaper.translateTo(this.RES_X/2, -this.RES_Y/2);
+		if (this.wallpaper2.getYCenter() >= this.RES_Y + this.RES_Y/2)
+			this.wallpaper2.translateTo(this.RES_X/2, -this.RES_Y/2);
 	}
 }
